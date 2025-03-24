8ffe527d23b821e67ec9449fbdc2d658eb59b464
FRANCIS KIOKO <57718524+frajosan97@users.noreply.github.com>
2025-03-24 11:45:32 +0300

    Edited

--- app/Http/Controllers/MpesaController.php
+++ app/Http/Controllers/MpesaController.php
@@ -2,21 +2,36 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\MpesaPayment;
-use App\Services\MpesaService;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
+use App\Services\MpesaService;
+use App\Services\SmsService;
+use Illuminate\Support\Facades\DB;
 
 class MpesaController extends Controller
 {
     protected $mpesaService;
+    protected $smsService;
 
-    public function __construct(MpesaService $mpesaService)
+    /**
+     * MpesaController constructor.
+     *
+     * @param MpesaService $mpesaService
+     * @param SmsService $smsService
+     */
+    public function __construct(MpesaService $mpesaService, SmsService $smsService)
     {
         $this->mpesaService = $mpesaService;
+        $this->smsService = $smsService;
     }
 
-    public function registerUrl(Request $request)
+    /**
+     * Register URLs for C2B transactions.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function registerUrl()
     {
         try {
             $response = $this->mpesaService->register();
@@ -34,7 +49,7 @@ class MpesaController extends Controller
         }
     }
 
-    public function stkpush(Request $request)
+    public function stkPush(Request $request)
     {
         try {
             $request->validate([
@@ -52,7 +67,7 @@ class MpesaController extends Controller
             $response = json_decode($response, true);
 
             if ($response['ResponseCode'] === '0') {
-                return response()->json(['success' => 'Payment initiated successfully, kindly advice the customer to check their phone and enter MPESA pin to complete the transaction']);
+                return response()->json(['success' => 'Payment initiated successfully, kindly check your phone and enter MPESA pin to complete the transaction']);
             }
         } catch (\Exception $exception) {
             Log::error('Error in ' . __METHOD__ . ' - File: ' . $exception->getFile() . ', Line: ' . $exception->getLine() . ', Message: ' . $exception->getMessage());
@@ -60,6 +75,12 @@ class MpesaController extends Controller
         }
     }
 
+    /**
+     * Handle validation request from M-Pesa.
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function validation(Request $request)
     {
         try {
@@ -69,65 +90,32 @@ class MpesaController extends Controller
             $decodedResponse = json_decode($rawInput, true);
 
             // Store raw input data in public/mpesa_logs
-            storeLog('validation', $decodedResponse);
+            storeLog('mpesa_logs/validation', $decodedResponse);
+
+            return response()->json(['ResultCode' => 0, 'ResultDesc' => 'Success']);
         } catch (\Exception $exception) {
             Log::error('Error in ' . __METHOD__ . ' - File: ' . $exception->getFile() . ', Line: ' . $exception->getLine() . ', Message: ' . $exception->getMessage());
-            return response()->json(['error' => $exception->getMessage()], 500);
+            return response()->json(['ResultCode' => 1, 'ResultDesc' => 'Error'], 500);
         }
     }
 
-    public function confirmation(Request $request)
-    {
-        try {
-            // Retrieve the raw JSON input
-            $rawInput = file_get_contents('php://input');
-            // Decode the JSON response
-            $decodedResponse = json_decode($rawInput, true);
-
-            // Check if the response is valid
-            if (!isset($decodedResponse['TransID']) || !isset($decodedResponse['TransAmount'])) {
-                Log::error('Invalid confirmation data.', ['raw_input' => $rawInput]);
-                return response()->json(['status' => 'error', 'message' => 'Invalid data'], 400);
-            }
-
-            // Extract data from the decoded response
-            $transactionId = $decodedResponse['TransID'];
-            $name = $decodedResponse['LastName'];
-            $amount = $decodedResponse['TransAmount'];
-            $phoneNumber = '0700000000';
-            $shortCode = $decodedResponse['BusinessShortCode'];
-            $status = 'success';
-
-            // Store successful payment response
-            MpesaPayment::create([
-                'transaction_id' => $transactionId,
-                'name' => $name,
-                'amount' => $amount,
-                'phone' => $phoneNumber,
-                'shortcode' => $shortCode,
-                'status' => $status,
-                'response_payload' => $decodedResponse,
-            ]);
-
-            // Store raw input data in public/mpesa_logs
-            storeLog('confirmation', $decodedResponse);
-        } catch (\Exception $exception) {
-            Log::error('Error in ' . __METHOD__ . ' - File: ' . $exception->getFile() . ', Line: ' . $exception->getLine() . ', Message: ' . $exception->getMessage());
-            return response()->json(['error' => $exception->getMessage()], 500);
-        }
-    }
-
+    /**
+     * Handle callback request from M-Pesa.
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function callback(Request $request)
     {
         try {
             // Retrieve the raw JSON input
             $rawInput = file_get_contents('php://input');
-            
+
             // Decode the JSON response
             $decodedResponse = json_decode($rawInput, true);
 
             // Store raw input data in public/mpesa_logs for debugging
-            storeLog('callback', $decodedResponse);
+            storeLog('mpesa_logs/callback', $decodedResponse);
 
             // Ensure the response structure is valid
             if (!isset($decodedResponse['Body']['stkCallback'])) {
@@ -143,39 +131,116 @@ class MpesaController extends Controller
 
             // Initialize transaction details
             $transactionId = null;
-            $name = null;
+            $firstName = null;
             $amount = null;
             $phoneNumber = null;
-            $shortCode = null;
-            $status = null;
 
             // Process successful transaction
             if ($resultCode === 0 && isset($callbackData['CallbackMetadata']['Item'])) {
                 $metadata = collect($callbackData['CallbackMetadata']['Item'])->pluck('Value', 'Name');
 
                 $transactionId = $metadata['MpesaReceiptNumber'] ?? null;
-                $name = $metadata['LastName'] ?? 'Unknown';
+                $firstName = $metadata['LastName'] ?? 'Unknown';
                 $amount = $metadata['Amount'] ?? null;
                 $phoneNumber = $metadata['PhoneNumber'] ?? null;
-                $shortCode = '650';
-                $status = 'success';
             }
 
-            // Store successful payment response
-            MpesaPayment::create([
+            // Store The Response to Database
+            $this->storeTransaction([
                 'transaction_id' => $transactionId,
-                'name' => $name,
+                'name' => $firstName,
                 'amount' => $amount,
                 'phone' => $phoneNumber,
-                'shortcode' => $shortCode,
-                'status' => $status,
-                'response_payload' => $decodedResponse,
+                'status' => 'success',
+                'transaction_type' => 'payment'
             ]);
 
-            return response()->json(['status' => 'success']);
+            return response()->json(['ResultCode' => 0, 'ResultDesc' => 'Success']);
         } catch (\Exception $exception) {
             Log::error('Error in ' . __METHOD__ . ' - File: ' . $exception->getFile() . ', Line: ' . $exception->getLine() . ', Message: ' . $exception->getMessage());
             return response()->json(['error' => $exception->getMessage()], 500);
+        }
+    }
+
+    /**
+     * Handle confirmation request from M-Pesa.
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function confirmation(Request $request)
+    {
+        try {
+            // Retrieve the raw JSON input
+            $rawInput = file_get_contents('php://input');
+            // Decode the JSON response
+            $decodedResponse = json_decode($rawInput, true);
+
+            // Store raw input data in public/mpesa_logs
+            storeLog('mpesa_logs/confirmation', $decodedResponse);
+
+            // Check if the response is valid
+            if (!isset($decodedResponse['TransID']) || !isset($decodedResponse['TransAmount']) || !isset($decodedResponse['MSISDN']) || !isset($decodedResponse['FirstName'])) {
+                Log::error('Invalid confirmation data.', ['raw_input' => $rawInput]);
+                return response()->json(['status' => 'error', 'message' => 'Invalid data'], 400);
+            }
+
+            // Extract necessary fields
+            $transactionId = $decodedResponse['TransID'];
+            $amount = $decodedResponse['TransAmount'];
+            $phoneNumber = $decodedResponse['MSISDN'];
+            $firstName = $decodedResponse['FirstName'];
+
+            // Store The Response to Database
+            $this->storeTransaction([
+                'transaction_id' => $transactionId,
+                'name' => $firstName,
+                'amount' => $amount,
+                'phone' => $phoneNumber,
+                'status' => 'success',
+                'transaction_type' => 'payment'
+            ]);
+
+            return response()->json(['ResultCode' => 0, 'ResultDesc' => 'Success']);
+        } catch (\Exception $exception) {
+            Log::error('Error in ' . __METHOD__ . ' - File: ' . $exception->getFile() . ', Line: ' . $exception->getLine() . ', Message: ' . $exception->getMessage());
+            return response()->json(['ResultCode' => 1, 'ResultDesc' => 'Error'], 500);
+        }
+    }
+
+    /**
+     * Store transaction details in the database.
+     *
+     * @param array $data
+     * @return void
+     */
+    protected function storeTransaction(array $data)
+    {
+        try {
+            // Start a database transaction
+            DB::beginTransaction();
+
+            // Store successful payment response
+            $mpesaPayment = MpesaPayment::create([
+                'transaction_id' => $data['transaction_id'],
+                'name' => $data['name'],
+                'amount' => $data['amount'],
+                'phone' => $data['phone'],
+                'shortcode' => '6570575',
+                'status' => $data['status'],
+                'response_payload' => json_encode($data),
+            ]);
+
+            if ($mpesaPayment) {
+                // Send notification
+            }
+
+            // Commit the transaction
+            DB::commit();
+        } catch (\Exception $e) {
+            // Rollback the transaction in case of an error
+            DB::rollBack();
+            Log::error('Error storing transaction: ' . $e->getMessage());
         }
     }
 }
